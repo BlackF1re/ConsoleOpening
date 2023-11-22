2df308ba1e0034534950a2fff06ea98733d96e3d
Павел Левицкий <creative-creative@outlook.com>
2023-11-23 02:16:57 +0700

    Removed useless code

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,33 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ConsoleOpening
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         [DllImport("kernel32.dll")]
         private static extern bool AllocConsole();
         [DllImport("kernel32.dll")]
-
         private static extern bool FreeConsole();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -47,7 +31,6 @@ namespace ConsoleOpening
             string text = Console.ReadLine();
             for (int i = 0; i < 10; i++)
             {
-                //Thread.Sleep(30);
                 Console.WriteLine(text + "  " + i);
             }
             Console.ReadKey();
